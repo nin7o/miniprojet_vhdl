8d1179e59e3c00a6c926464250dc36e23e868855
nin7o <nin7o.g@proton.me>
2023-11-19 18:36:58 +0000

    master working

--- fournitures/OpL/MasterOpl.vhd
+++ fournitures/OpL/MasterOpl.vhd
@@ -5,20 +5,207 @@ entity MasterOpl is
   port ( rst : in std_logic;
          clk : in std_logic;
          en : in std_logic;
-         v1 : in std_logic_vector (7 downto 0);
-         v2 : in std_logic_vector(7 downto 0);
+         v1 : in std_logic_vector (7 downto 0); -- premier octet opérande
+         v2 : in std_logic_vector(7 downto 0); -- second octet opérande
          miso : in std_logic;
          ss   : out std_logic;
          sclk : out std_logic;
          mosi : out std_logic;
-         val_nand : out std_logic_vector (7 downto 0);
-         val_nor : out std_logic_vector (7 downto 0);
-         val_xor : out std_logic_vector (7 downto 0);
+         val_nand : out std_logic_vector (7 downto 0); -- résultat du nand, premier octet reçu
+         val_nor : out std_logic_vector (7 downto 0); -- résultat du nor, second octet reçu
+         val_xor : out std_logic_vector (7 downto 0); -- résultat du xor, troisième octet reçu
          busy : out std_logic);
 end MasterOpl;
 
 architecture behavior of MasterOpl is
 
+	-- Ajout du composant er_1octet
+	COMPONENT er_1octet
+	PORT(
+		rst : IN std_logic;
+		clk : IN std_logic;
+		en : IN std_logic;
+		din : IN std_logic_vector(7 downto 0);
+		miso : IN std_logic;          
+		sclk : OUT std_logic;
+		mosi : OUT std_logic;
+		dout : OUT std_logic_vector(7 downto 0);
+		busy : OUT std_logic
+		);
+	END COMPONENT;
+	-- ce composant est utilisé pour envoyer et recevoir un octet
+
+
+	type t_etat is (idle, attente, echange);
+	signal etat : t_etat;
+
+	-- input du composant er_1octet
+	signal en_er_1octet : std_logic;
+	signal din_er_1octet : std_logic_vector(7 downto 0);
+
+	-- output du composant er_1octet
+	signal busy_er_1octet : std_logic;
+	signal dout_er_1octet : std_logic_vector(7 downto 0);
+
 begin
+
+	-- Instanciation du composant er_1octet
+	Inst_er_1octet : er_1octet PORT MAP(
+		-- signaux en sortie directe
+		sclk => sclk,
+		miso => miso,
+		mosi => mosi,
+		-- signaux en entrée directe
+		clk => clk,
+		rst => rst,
+		-- signaux modifiés par le composant
+		en => en_er_1octet,
+		din => din_er_1octet,
+		dout => dout_er_1octet,
+		busy => busy_er_1octet
+		);
+
+process(clk, rst)
+	variable cpt_clk : natural;
+	variable cpt_bytes : natural;
+	
+begin
+
+	if (rst = '0') then
+
+		-- réinitialisaation des variables du processus
+		-- et des signaux modifiés par le processus
+
+		-- réinitialisation du compteur
+		cpt_clk := 20;
+
+		-- réinitialisation du signal busy
+		busy <= '0';
+
+		-- réinitialisation du signal ss
+		ss <= '1';
+
+		-- réinitialisation de l'octet courant 
+		cpt_bytes := 3;
+
+		-- réinitialisation de l'etat
+		etat <= idle;
+
+	elsif(rising_edge(clk)) then
+
+		-- front montant de l'horloge
+		case etat is 
+
+			-- attente d'un ordre
+			when idle =>
+
+				if(en = '1') then
+					-- un ordre est détecté
+					-- on signale qu'on est occupé
+					busy <= '1';
+
+					-- initialisation du signal ss pour la transmission spi
+					ss <= '0';
+
+					-- on initialise le compteur de clk
+					cpt_clk := 20;
+
+					-- on initialise le compteur d'octets envoyés
+					cpt_bytes := 3;
+
+					-- on passe à l'état d'attente
+					etat <= attente;
+
+				else
+					--aucun ordre, on ne fait rien
+					null;
+				end if;
+
+			when attente =>
+
+					-- une attente de 20 cycles d'horloge clk est nécessaire
+					-- pour que le slave soit prêt à échanger,
+					-- et de 5 cycle entre chaque octet échangé
+
+					if(cpt_clk > 0) then
+						-- l'attente n'est pas terminée
+						cpt_clk := cpt_clk - 1;
+
+					else
+						-- l'attente est terminée
+
+						-- on change d'octet à envoyer
+						cpt_bytes := cpt_bytes - 1;
+
+						-- on passe le bon entier en entrée du composant er_1octet
+
+						case cpt_bytes is
+							when 2 =>
+								-- on envoie le premier octet opérande 1
+								din_er_1octet <= v1;
+							when 1 =>
+								-- on envoie le premier octet opérande 2
+								din_er_1octet <= v2;
+							when 0 =>
+								-- on n'envoie rien
+								din_er_1octet <= (others => 'U');
+							when others => null;
+						end case;	
+
+						-- on lance l'échange d'un octet en activant le sous composant
+						en_er_1octet <= '1';
+						etat <= echange;
+					end if;
+
+			when echange =>
+					-- on echange un bit à la fois via le composant er_1octet
+
+					-- on rabaissee l'ordre passé au sous composant
+					en_er_1octet <= '0';
+					din_er_1octet <= (others => 'U');
+
+					if (en_er_1octet = '0' and busy_er_1octet = '0') then
+						-- échange d'un octet terminé
+						-- on récupère l'octet reçu et on le stocke dans le bon signal
+
+						case cpt_bytes is
+							when 2 =>
+								-- on récupère le premier octet reçu
+								val_nand <= dout_er_1octet;
+							when 1 =>
+								-- on récupère le second octet reçu
+								val_nor <= dout_er_1octet;
+							when 0 =>
+								-- on récupère le troisième octet reçu
+								val_xor <= dout_er_1octet;
+							when others => null;
+						end case;
+					
+						-- on prépare la suite
+						if(cpt_bytes > 0) then
+							-- il reste des octets à échanger
+							-- on passe l'attente à 5 cycles d'horloge
+							cpt_clk := 5;
+							etat <= attente;
+						else
+							-- tous les octets ont été envoyés
+							-- on signale qu'on n'est plus occupé
+							busy <= '0';
+
+							-- on reset le slave
+							ss <= '1';
+
+							-- on revient sur l'etat idle
+							etat <= idle;
+						end if;
+					end if;
+									
+			when others =>
+				-- ne devrait jamais arriver
+				etat <= idle;
+		
+		end case;
+	end if;
+end process;
 
 end behavior;
--- fournitures/OpL/TestOpl.vhd
+++ fournitures/OpL/TestOpl.vhd
@@ -1,2 +1,196 @@
--- fichier à écraser avec le fichier de test généré par ISE (New Source)
--- le nom du composant de test doit être TestOpl (respect minuscules/majuscules)
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   10:51:48 11/16/2023
+-- Design Name:   
+-- Module Name:   /home/mricard/Xilink/mini_projet/testMasterOpl.vhd
+-- Project Name:  mini_projet
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: MasterOpl
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY testMasterOpl IS
+END testMasterOpl;
+ 
+ARCHITECTURE behavior OF testMasterOpl IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT MasterOpl
+    PORT(
+         rst : IN  std_logic;
+         clk : IN  std_logic;
+         en : IN  std_logic;
+         v1 : IN  std_logic_vector(7 downto 0);
+         v2 : IN  std_logic_vector(7 downto 0);
+         miso : IN  std_logic;
+         ss : OUT  std_logic;
+         sclk : OUT  std_logic;
+         mosi : OUT  std_logic;
+         val_nand : OUT  std_logic_vector(7 downto 0);
+         val_nor : OUT  std_logic_vector(7 downto 0);
+         val_xor : OUT  std_logic_vector(7 downto 0);
+         busy : OUT  std_logic
+        );
+    END COMPONENT;
+    COMPONENT SlaveOpl
+	 PORT(
+		sclk : IN std_logic;
+		mosi : IN std_logic;
+		ss : IN std_logic;          
+		miso : OUT std_logic
+		);
+	 END COMPONENT;
+
+   --Inputs
+   signal rst : std_logic := '0';
+   signal clk : std_logic := '0';
+   signal en : std_logic := '0';
+   signal v1 : std_logic_vector(7 downto 0) := (others => '0');
+   signal v2 : std_logic_vector(7 downto 0) := (others => '0');
+   signal miso : std_logic := '0';
+
+ 	--Outputs
+   signal ss : std_logic;
+   signal sclk : std_logic;
+   signal mosi : std_logic;
+   signal val_nand : std_logic_vector(7 downto 0);
+   signal val_nor : std_logic_vector(7 downto 0);
+   signal val_xor : std_logic_vector(7 downto 0);
+   signal busy : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+   constant sclk_period : time := 10 ns;
+ 
+   -- signaux attendu pour la comparaison
+   signal ref_nand : std_logic_vector(7 downto 0);
+   signal ref_nor: std_logic_vector(7 downto 0);
+   signal ref_xor: std_logic_vector(7 downto 0);
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: MasterOpl PORT MAP (
+          rst => rst,
+          clk => clk,
+          en => en,
+          v1 => v1,
+          v2 => v2,
+          miso => miso,
+          ss => ss,
+          sclk => sclk,
+          mosi => mosi,
+          val_nand => val_nand,
+          val_nor => val_nor,
+          val_xor => val_xor,
+          busy => busy
+        );
+		  
+	Inst_SlaveOpl: SlaveOpl PORT MAP(
+			sclk => sclk,
+			mosi => mosi,
+			miso => miso,
+			ss => ss
+		);
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 		
+	v1   <= "UUUUUUUU" after 0 ps,
+			  "01100001" after  200000 ps,
+           "10100101" after 1080000 ps,
+           "11111111" after 1960000 ps,
+			  "10100101" after 2840000 ps,
+			  "10010110" after 3720000 ps;
+
+				  
+	v2   <= "UUUUUUUU" after 0 ps,
+	        "01000100" after  200000 ps,
+           "11010010" after 1080000 ps,
+           "00000000" after 1960000 ps,
+			  "10010100" after 2840000 ps,
+			  "00111011" after 3720000 ps;
+
+   -- Idealément la simulation fait aux alentour de 6us
+	ref_nand <= "00110011" after 595000 ps,
+					"10111111" after 1475000 ps,
+ 					"01111111" after 2355000 ps,
+					"11111111" after 3235000 ps,
+					"01111011" after 4115000 ps,
+					"11101101" after 4995000 ps;
+
+	ref_nor  <= "11001100" after 835000 ps,
+					"10011010" after 1715000 ps,
+ 					"00001000" after 2595000 ps,
+					"00000000" after 3475000 ps,
+					"01001010" after 4355000 ps,
+					"01000000" after 5235000 ps;
+
+	ref_xor  <= "10100000" after 1075000 ps,
+				 	"00100101" after 1955000 ps,
+ 					"01110111" after 2835000 ps,
+					"11111111" after 3715000 ps,
+					"00110001" after 4595000 ps,
+					"10101101" after 5475000 ps;
+					
+					
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		
+		
+      wait for clk_period*10;
+
+		rst <= '1';
+		en <= '1';
+		-- insert stimulus here 
+
+      wait;
+   end process;
+
+	process(clk)
+	-- permet de vérifier que les valeurs de nand, nor et xor sont les bonnes
+	begin
+    if(falling_edge(clk)) then
+      assert(val_nand = ref_nand) report "nand faux"
+      severity error;
+      assert(val_nor = ref_nor) report "nor faux"
+      severity error;
+      assert(val_xor = ref_xor) report "xor faux"
+      severity error;
+    end if;
+	end process;
+END;
